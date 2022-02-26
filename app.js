81a35523ec0625fad5aae21dc08ad69bb940a6f1
Jyun-Fu-Chen <than3698741@gmail.com>
2022-02-26 20:26:06 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require("express")
 const port = 3000
 const exphbs = require("express-handlebars")
+const app = express()
+//use express-handlebars
+app.engine("handlebars",exphbs.engine())
+app.set('view engine', 'handlebars')
+app.set('views','./views')
+
+
+
+
+app.get('/',(req,res)=>{
+  res.send('This is todo_list website')
+})
+
+app.listen(port,()=>{
+  console.log(`localhost:${port} is running`)
+})
